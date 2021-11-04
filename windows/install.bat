0c0a3808b6681a4598fa6feca7e70f5d5a5795ed
ghost <ghost@outlook.com>
2021-11-04 18:43:42 +0800

    change windows

--- windows/install.bat
+++ windows/install.bat
@@ -2,3 +2,32 @@
 
 #pagram
 choco install tortoisesvn -y
+choco install git -y
+choco install notepadplusplus -y
+choco install 7zip -y
+choco install telegram -y
+choco install python2 -y
+choco install python -version 3.6.8 -y
+choco install jdk8 -y    
+choco install GoogleChrome -y
+choco install firefox -y
+choco install beyondcompare -y
+choco install filezilla -y
+choco install evernote -y
+choco install wps-office-free -y
+
+choco install MobaXterm -y
+choco install vmwareworkstation -y      #(15pro key ZC10K-8EF57-084QZ-VXYXE-ZF2XF)
+
+choco install emacs -y
+choco install vscode -y
+choco install msys2 -y
+choco install pycharm -y
+choco install intellijidea-ultimate -y
+choco install WebStorm -y
+choco install androidstudio -y
+choco install RubyMine -y
+choco install goland -y
+choco install eclipse -y
+choco install intellijidea-ultimate -y
+choco install microsoft-windows-terminal -y
